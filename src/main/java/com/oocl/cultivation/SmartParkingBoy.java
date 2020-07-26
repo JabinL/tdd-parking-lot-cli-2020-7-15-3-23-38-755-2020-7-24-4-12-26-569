92fa2ffdeb071f0d023487bbbf367dd29e0ed8e7
Jabin <2099008974@qq.com>
2020-07-26 10:20:08 +0800

    [Parking Lot][Jabin] test: add should_choose_P2_parkingLot_when_smartparkingboy_parking_given_3_parkinglots_P2_has_most_contains

---
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -0,0 +1,9 @@
+package com.oocl.cultivation;
+
+public class SmartParkingBoy  extends ParkingBoy{
+    public SmartParkingBoy(ParkingLot parkingLot) {
+        super(parkingLot);
+    }
+
+
+}
--- src/test/java/com/oocl/cultivation/test/ParkingTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingTest.java
@@ -1,9 +1,7 @@
 package com.oocl.cultivation.test;
 
-import com.oocl.cultivation.Car;
-import com.oocl.cultivation.ParkingBoy;
-import com.oocl.cultivation.ParkingLot;
-import com.oocl.cultivation.Ticket;
+import com.oocl.cultivation.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
@@ -208,6 +206,97 @@ class ParkingTest {
         assertEquals(true, isTipsRight);
     }
 
-    
+    @Test
+    public void should_print_tips_when_packingboy_parking_given_parkinglot_is_full() {
+
+        //given
+        Car car = new Car("C11");
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        for (int i = 0; i < 10; i++) {
+            Car car0 = new Car("C" + i);
+            parkingBoy.parking(car0);
+        }
+        //when
+        Ticket ticket = parkingBoy.parking(car);
+        //then
+        boolean isTipsRight = false;
+        if (systemOut().endsWith("Not enough position.\n")) {
+            isTipsRight = true;
+        }
+
+        assertEquals(true, isTipsRight);
+    }
+
+    @Test
+    public void should_return_ticket_when_parkingboy_parking_given_2_parkinglots_1_is_full() {
+        //given
+        Car car = new Car("C11");
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        for (int i = 0; i < 10; i++) {
+            Car car0 = new Car("C" + i);
+            parkingBoy.parking(car0);
+        }
+        parkingBoy.addParkingLot(new ParkingLot());
+        //when
+        Ticket ticket = parkingBoy.parking(car);
+        //then
+        boolean isTicketNull = false;
+        if (ticket == null) {
+            isTicketNull = true;
+        }
+
+        assertEquals(false, isTicketNull);
+    }
+    @Test
+    public void should_choose_P2_parkingLot_when_smartparkingboy_parking_given_3_parkinglots_P2_has_most_contains() {
+        //given
+        Car car = new Car("C2");
+
+        ParkingLot parkingLot_1 = new ParkingLot();
+        List<Ticket> ticketList_1 = new ArrayList<>();
+        List<Car> carList_1 = new ArrayList<>();
+        ticketList_1.add(new Ticket("T1","C1","P1"));
+        ticketList_1.add(new Ticket("T1","C2","P1"));
+        carList_1.add(new Car("C1"));
+        carList_1.add(new Car("C2"));
+        parkingLot_1.setTicketList(ticketList_1);
+        parkingLot_1.setCarList(carList_1);
+
+
+        ParkingLot parkingLot_2 = new ParkingLot();
+        List<Ticket> ticketList_2 = new ArrayList<>();
+        List<Car> carList_2 = new ArrayList<>();
+        ticketList_2.add(new Ticket("T1","C1","P2"));
+        carList_2.add(new Car("C1"));
+        parkingLot_2.setTicketList(ticketList_2);
+        parkingLot_2.setCarList(carList_2);
+
+
+        ParkingLot parkingLot_3 = new ParkingLot();
+        List<Ticket> ticketList_3 = new ArrayList<>();
+        List<Car> carList_3 = new ArrayList<>();
+        ticketList_3.add(new Ticket("T1","C1","P3"));
+        ticketList_3.add(new Ticket("T2","C2","P3"));
+        ticketList_3.add(new Ticket("T3","C3","P3"));
+        carList_3.add(new Car("C1"));
+        carList_3.add(new Car("C2"));
+        carList_3.add(new Car("C3"));
+
+        SmartParkingBoy parkingBoy = new SmartParkingBoy(parkingLot_1);
+        parkingBoy.addParkingLot(parkingLot_2);
+        parkingBoy.addParkingLot(parkingLot_3);
+        //when
+        Ticket ticket = parkingBoy.parking(car);
+        //then
+        boolean isTicketOfP2 = false;
+        if (ticket.getParkingLotId().equals("P2")) {
+            isTicketOfP2 = true;
+        }
+
+        assertEquals(true, isTicketOfP2);
+    }
+
 
 }
